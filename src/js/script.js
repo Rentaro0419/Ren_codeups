dce60ce8a1f9c02d6c23e9dc8130f27be8f5e89c
Rentaro0419 <webrenf220912@gmail.com>
2023-07-23 10:02:21 +0900

    修正2

--- src/js/script.js
+++ src/js/script.js
@@ -47,8 +47,8 @@ const swiper = new Swiper(".js-campaign-swiper", {
   loop: true,
   speed: 1000,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
   },
   autoplay: {
     delay: 3000,
@@ -82,49 +82,6 @@ const swiper = new Swiper(".js-campaign-swiper", {
   },
 });
 
-// ページトップボタン
-$(function () {
-  var pageTop = $(".pagetop");
-  pageTop.hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      pageTop.fadeIn();
-    } else {
-      pageTop.fadeOut();
-    }
-  });
-  pageTop.click(function () {
-    $("body,html").animate(
-      {
-        scrollTop: 0,
-      },
-      500
-    );
-    return false;
-  });
-  //フッター手前でストップ
-  $(".pagetop").hide();
-  $(window).on("scroll", function () {
-    var scrollHeight = $(document).height();
-    console.log(scrollHeight);
-    //読み込んだページの全高さ
-    var scrollPosition = $(window).height() + $(window).scrollTop();
-    //ウィンドウの高さ + スクロール位置 =現在のスクロール位置
-    var footHeight = $(".footer").innerHeight();
-    if (scrollHeight - scrollPosition <= footHeight) {
-      //ページトップボタンがフッター手前に来たらpositionとfixedからabsoluteに変更
-      $(".pagetop").css({
-        position: "absolute",
-        bottom: footHeight + 20,
-      });
-    } else {
-      $(".pagetop").css({
-        position: "fixed",
-        bottom: "20px",
-      });
-    }
-  });
-});
 
 
 //画像アニメーション
@@ -158,3 +115,52 @@ var box = $(".js-colorbox"),
     }
   });
 });
+
+
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+
+  var scroll = $(window).scrollTop(); //スクロール値を取得
+  if (scroll >= 200){//200pxスクロールしたら
+    $('#page-top').removeClass('DownMove');		// DownMoveというクラス名を除去して
+    $('#page-top').addClass('UpMove');			// UpMoveというクラス名を追加して出現
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名が既に付与されていたら
+      $('#page-top').removeClass('UpMove');	//  UpMoveというクラス名を除去し
+      $('#page-top').addClass('DownMove');	// DownMoveというクラス名を追加して非表示
+    }
+  }
+  
+  var wH = window.innerHeight; //画面の高さを取得
+  var footerPos =  $('#footer').offset().top; //footerの位置を取得
+  if(scroll+wH >= (footerPos+10)) {
+    var pos = (scroll+wH) - footerPos+10 //スクロールの値＋画面の高さからfooterの位置＋10pxを引いた場所を取得し
+    $('#page-top').css('bottom',pos);	//#page-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+  }else{//それ以外は
+    if($('#page-top').hasClass('UpMove')){//UpMoveというクラス名がついていたら
+      $('#page-top').css('bottom','10px');// 下から10pxの位置にページリンクを指定
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
